073647830fc2022c4842630d524a7682ed70e257
NiroDev <niros.email@gmail.com>
2024-07-22 11:14:09 +0200

    Finished tongue hitting behavior and first draft for networking
    moved DropBlood method

--- LethalMon/Behaviours/KidnapperFoxTamedBehaviour.cs
+++ LethalMon/Behaviours/KidnapperFoxTamedBehaviour.cs
@@ -1,12 +1,14 @@
 ﻿using GameNetcodeStuff;
 using HarmonyLib;
-using LethalLib.Modules;
 using LethalMon.Items;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
+using Random = UnityEngine.Random;
 using Vector3 = UnityEngine.Vector3;
 
 namespace LethalMon.Behaviours
@@ -38,7 +40,7 @@ namespace LethalMon.Behaviours
             }
         }
 
-        internal static NetworkList<ulong> hidingPlayers = new NetworkList<ulong>();
+        internal static List<ulong> hidingPlayers = [];
 
         internal List<GameObject> hidingSpores = [];
         internal readonly int MaximumHidingSpores = 3;
@@ -46,6 +48,15 @@ namespace LethalMon.Behaviours
 
         internal readonly float IdleTimeTillSpawningSpore = 3f;
         internal float idleTime = 0f;
+
+
+        private DateTime canTongueHitAfter = new DateTime(0);
+        internal readonly float TongueHitCooldownSeconds = 2f;
+        internal Coroutine? tongueShootCoroutine = null;
+        internal Coroutine? pushTargetCoroutine = null;
+        internal readonly int TongueKillPercentage = 20;
+        internal EnemyAI? lastHitEnemy = null;
+        internal int enemyHitTimes = 0;
         #endregion
 
         #region Action Keys
@@ -63,13 +74,6 @@ namespace LethalMon.Behaviours
             Fox.creatureAnimator.SetBool("ShootTongue", value: tongueOut);
             tongueOut = !tongueOut;
             Fox.creatureAnimator.SetBool("mouthOpen", Vector3.Distance(Fox.transform.position, ownerPlayer!.transform.position) < 3f);
-           // Fox.creatureAnimator.SetBool("ReelingPlayerIn", value: true);
-
-            /*Fox.creatureAnimator.SetTrigger("MatingCall");
-            int num = Random.Range(0, Fox.callsClose.Length);
-            Fox.callClose.PlayOneShot(Fox.callsClose[num]);
-            Fox.callFar.PlayOneShot(Fox.callsFar[num]);
-            RoundManager.Instance.PlayAudibleNoise(base.transform.position, 20f, 0.6f, 0, noiseIsInsideClosedShip: false, 245403);*/
         }
         #endregion
 
@@ -113,9 +117,11 @@ namespace LethalMon.Behaviours
                         Fox.agent.enabled = true;
                         Fox.agent.speed = 5f;
                     }
+                    //Fox.tongueTarget = ownerPlayer == Utils.CurrentPlayer ? ownerPlayer!.upperSpineLocalPoint : ownerPlayer!.upperSpine;
                     break;
 
                 case TamingBehaviour.TamedDefending:
+                    ShootTongueAtEnemyServerRpc();
                     break;
 
                 default: break;
@@ -127,12 +133,10 @@ namespace LethalMon.Behaviours
             // OWNER ONLY
             base.OnTamedFollowing();
 
-            if(Fox.backedUpFromWatchingPlayer )
-
             Fox.CalculateAnimationDirection(Fox.maxAnimSpeed);
-            Fox.AddProceduralOffsetToLimbsOverTerrain();
+            Fox.LateUpdate(); // Fox.AddProceduralOffsetToLimbsOverTerrain();
 
-            if(Fox.agentLocalVelocity.x == 0f && Fox.agentLocalVelocity.z == 0f) // Idle
+            if (Fox.agentLocalVelocity.x == 0f && Fox.agentLocalVelocity.z == 0f) // Idle
             {
                 idleTime += Time.deltaTime;
                 if (idleTime > IdleTimeTillSpawningSpore)
@@ -147,11 +151,26 @@ namespace LethalMon.Behaviours
             }
             else
                 idleTime = 0f;
+
+            if (canTongueHitAfter < DateTime.Now)
+                TargetNearestEnemy();
         }
 
         internal override void OnTamedDefending()
         {
             // OWNER ONLY
+            if ((targetEnemy == null || targetEnemy.isEnemyDead) && tongueShootCoroutine != null)
+            {
+                StopCoroutine(tongueShootCoroutine);
+                tongueShootCoroutine = null;
+
+                targetEnemy = null;
+                Fox.creatureAnimator.SetBool("shootTongue", false);
+                Fox.spitParticle.Stop();
+
+                SwitchToTamingBehaviour(TamingBehaviour.TamedFollowing);
+            }
+
             base.OnTamedDefending();
         }
 
@@ -161,17 +180,247 @@ namespace LethalMon.Behaviours
             base.OnEscapedFromBall(playerWhoThrewBall);
         }
 
+        internal override void OnCallFromBall()
+        {
+            base.OnCallFromBall();
+
+            Fox.EnableEnemyMesh(true);
+        }
+
         public override PokeballItem? RetrieveInBall(Vector3 position)
         {
             // ANY CLIENT
             for (int i = hidingSpores.Count - 1; i >= 0; --i)
                 DestroyHidingSpore(hidingSpores[i]);
 
+            if (tongueShootCoroutine != null)
+                StopCoroutine(tongueShootCoroutine);
+
+            if (pushTargetCoroutine != null)
+                StopCoroutine(pushTargetCoroutine);
+
             return base.RetrieveInBall(position);
+        }
+
+        internal override void TurnTowardsPosition(Vector3 position)
+        {
+            //base.TurnTowardsPosition(position);
+            Fox.LookAtPosition(position);
         }
         #endregion
 
-        #region Methods
+        #region TongueShooting
+        [ServerRpc(RequireOwnership = false)]
+        internal void ShootTongueAtEnemyServerRpc()
+        {
+            if (targetEnemy == null || tongueShootCoroutine != null || canTongueHitAfter >= DateTime.Now) return;
+
+            var killEnemy = targetEnemy.enemyType.canDie && (enemyHitTimes >= 3 || Random.Range(0, 100) < TongueKillPercentage);
+
+            tongueShootCoroutine = StartCoroutine(ShootTongueAtEnemyCoroutine(killEnemy));
+
+            if (killEnemy)
+            {
+                lastHitEnemy = null;
+                enemyHitTimes = 0;
+            }
+            else if (lastHitEnemy == targetEnemy)
+                enemyHitTimes++;
+            else
+            {
+                lastHitEnemy = targetEnemy;
+                enemyHitTimes = 1;
+            }
+        }
+
+        internal IEnumerator ShootTongueAtEnemyCoroutine(bool howlAndKill = false)
+        {
+            if (targetEnemy == null)
+            {
+                tongueShootCoroutine = null;
+                yield break;
+            }
+
+            LethalMon.Log("ShootTongueAtEnemy");
+
+            if (howlAndKill)
+            {
+                Fox.DoMatingCall();
+                float timer = 0f;
+                while (timer < 1.5f)
+                {
+                    timer += Time.deltaTime;
+                    Fox.LookAtPosition(targetEnemy.transform.position);
+                    yield return null;
+                }
+            }
+
+            if (howlAndKill)
+            {
+                int hits = 0;
+                while (!targetEnemy.isEnemyDead && hits < 3)
+                {
+                    PushTargetEnemyWithTongueClientRpc(1);
+                    yield return new WaitWhile(() => pushTargetCoroutine != null);
+                    hits++;
+                }
+
+                if (!targetEnemy.isEnemyDead) // Finisher
+                {
+                    PushTargetEnemyWithTongueClientRpc(targetEnemy.enemyHP);
+                    yield return new WaitWhile(() => pushTargetCoroutine != null);
+                }
+            }
+            else
+            {
+                PushTargetEnemyWithTongueClientRpc(); // warning shot
+                yield return new WaitWhile(() => pushTargetCoroutine != null);
+            }
+
+            targetEnemy = null;
+
+            LethalMon.Log("ShootTongueAtEnemy -> Finished coroutine");
+            tongueShootCoroutine = null;
+            canTongueHitAfter = DateTime.Now.AddSeconds(TongueHitCooldownSeconds);
+            SwitchToTamingBehaviour(TamingBehaviour.TamedFollowing);
+
+        }
+
+        [ClientRpc]
+        internal void PushTargetEnemyWithTongueClientRpc(int damageOnHit = 0)
+        {
+            if (pushTargetCoroutine != null)
+                StopCoroutine(pushTargetCoroutine);
+
+            pushTargetCoroutine = StartCoroutine(PushTargetEnemyWithTongue(damageOnHit));
+        }
+
+        internal IEnumerator PushTargetEnemyWithTongue(int damageOnHit = 0)
+        {
+            if (targetEnemy == null)
+            {
+                pushTargetCoroutine = null;
+                yield break;
+            }
+
+            LethalMon.Log("PushTargetEnemyWithTongue started.", LethalMon.LogType.Warning);
+            // Calculate enemy push path
+            var force = Mathf.Min(10f - Vector3.Distance(Fox.transform.position, targetEnemy.transform.position), 3f) * 10f; // smaller distance = larger flight
+            if (Utils.TryGetRealEnemyBounds(targetEnemy, out Bounds enemyBounds))
+            {
+                LethalMon.Log("ENEMY HEIGHT: " + enemyBounds.size.y);
+                force /= enemyBounds.size.y; // Larger enemies are less affected
+            }
+
+            var direction = (targetEnemy.transform.position - Fox.transform.position).normalized;
+            yield return PushTargetEnemyWithTongue(direction, force, damageOnHit);
+            LethalMon.Log("PushTargetEnemyWithTongue finished.", LethalMon.LogType.Warning);
+            pushTargetCoroutine = null;
+        }
+
+        internal IEnumerator PushTargetEnemyWithTongue(Vector3 direction, float force, int damageOnHit = 0)
+        {
+            if (targetEnemy == null) yield break;
+
+            yield return ReachTargetEnemyWithTongue();
+
+            LethalMon.Log("PushTargetEnemyWithTongue in direction " + direction + " with a force of " + force);
+
+            var startPosition = targetEnemy.transform.position;
+            var landingPosition = targetEnemy.transform.position + direction * force;
+
+            var distanceShortenedByWall = 0f;
+            if (Physics.Linecast(startPosition + Vector3.up, landingPosition + Vector3.up, out RaycastHit hit, StartOfRound.Instance.collidersAndRoomMaskAndDefault, QueryTriggerInteraction.Ignore))
+            {
+                // Obstacle in the way
+                distanceShortenedByWall = Vector3.Distance(landingPosition, hit.point);
+                landingPosition = hit.point - direction * 0.5f - Vector3.up;
+            }
+
+            targetEnemy.enabled = false;
+            targetEnemy.agent.enabled = false;
+
+            if (damageOnHit > 0)
+            {
+                DropBlood(targetEnemy.transform.position, damageOnHit, damageOnHit);
+                targetEnemy.HitEnemy(damageOnHit, null, true);
+            }
+            else
+            {
+                targetEnemy.creatureSFX.PlayOneShot(targetEnemy.enemyType.hitBodySFX);
+                WalkieTalkie.TransmitOneShotAudio(targetEnemy.creatureSFX, targetEnemy.enemyType.hitBodySFX);
+            }
+
+            var totalFlyingTime = force / 20f;
+            yield return PushTargetEnemyTo(landingPosition, totalFlyingTime, Fox.LateUpdate); // LateUpdate to update tongue
+
+            if (distanceShortenedByWall > 0f)
+            {
+                var timer = 0f;
+                var timeStunned = Mathf.Min(distanceShortenedByWall / 10f, 0.3f);
+                LethalMon.Log("PushTargetEnemyWithTongue -> Hit wall. Stunning for " + timeStunned);
+                while (timer < timeStunned)
+                {
+                    timer += Time.deltaTime;
+                    if (Fox.tongueLengthNormalized > 0f)
+                        Fox.LateUpdate();
+                    yield return null;
+                }
+            }
+            while (Fox.tongueLengthNormalized > 0f && Fox.tongueTarget == null)
+            {
+                Fox.LateUpdate();
+                yield return null;
+            }
+
+            if (targetEnemy != null && !targetEnemy.isEnemyDead)
+            {
+                if (targetEnemy.agent != null)
+                    targetEnemy.agent.enabled = true;
+                targetEnemy.enabled = true;
+            }
+
+            Fox.tongueLengthNormalized = 0f;
+            Fox.LateUpdate(); // Update tongue
+
+            Fox.spitParticle.Stop();
+            LethalMon.Log("PushTargetEnemyWithTongue -> Parameterised coroutine finished");
+        }
+
+        internal IEnumerator ReachTargetEnemyWithTongue()
+        {
+            if (targetEnemy == null) yield break;
+
+            Fox.creatureAnimator.SetBool("ShootTongue", value: true);
+            Fox.creatureVoice.PlayOneShot(Fox.shootTongueSFX);
+            Fox.tongueLengthNormalized = 0f;
+            Fox.tongueTarget = targetEnemy!.transform;
+
+            while (Fox.tongueLengthNormalized < 1f)
+            {
+                Fox.LookAtPosition(targetEnemy.transform.position);
+                Fox.LateUpdate(); // to update the tongue
+                yield return null;
+            }
+
+            Fox.creatureAnimator.SetBool("ShootTongue", value: false);
+            Fox.tongueTarget = null;
+        }
+
+        internal IEnumerator PushTargetEnemyTo(Vector3 targetPosition, float duration, Action? onUpdate)
+        {
+            if (targetEnemy == null) yield break;
+
+            var startPosition = targetEnemy.transform.position;
+            var timer = 0f;
+            while (timer < duration)
+            {
+                timer += Time.deltaTime;
+                onUpdate?.Invoke();
+                TeleportEnemy(targetEnemy, Vector3.Lerp(startPosition, targetPosition, timer / duration));
+                yield return null;
+            }
+        }
         #endregion
 
         #region HideSpores
@@ -240,6 +489,12 @@ namespace LethalMon.Behaviours
         [ServerRpc(RequireOwnership = false)]
         internal void SetPlayerHiddenInMoldServerRpc(ulong playerID, bool hide = true)
         {
+            SetPlayerHiddenInMoldClientRpc(playerID, hide);
+        }
+
+        [ClientRpc]
+        internal void SetPlayerHiddenInMoldClientRpc(ulong playerID, bool hide = true)
+        {
             if (hide)
             {
                 hidingPlayers.Add(playerID);
@@ -295,6 +550,18 @@ namespace LethalMon.Behaviours
                     UnhideLocalPlayer();
             }
 
+            private void OnDestroy()
+            {
+                if (localPlayerHiding)
+                    UnhideLocalPlayer();
+            }
+
+            private void OnDisable()
+            {
+                if (localPlayerHiding)
+                    UnhideLocalPlayer();
+            }
+
             private void HideLocalPlayer()
             {
                 localPlayerHiding = true;
@@ -316,7 +583,7 @@ namespace LethalMon.Behaviours
         [HarmonyPostfix]
         public static bool PlayerIsTargetablePostfix(bool __result, EnemyAI __instance, PlayerControllerB playerScript)
         {
-            if (hidingPlayers.Contains(playerScript.playerClientId) && Vector3.Distance(__instance.transform.position, playerScript.transform.position) > 3f)
+            if (hidingPlayers != null && hidingPlayers.Contains(playerScript.playerClientId) && Vector3.Distance(__instance.transform.position, playerScript.transform.position) > 3f)
             {
                 LethalMon.Log("Player inside hiding spore. Not targetable.");
                 return false;
