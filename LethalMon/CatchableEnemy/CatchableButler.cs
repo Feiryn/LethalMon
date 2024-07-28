630f99d1310451173c1c4a1e66a72a8a616f8486
Feiryn <98115666+feiryn@users.noreply.github.com>
2024-07-28 16:15:13 +0200

    Stop butler music when being captured

--- LethalMon/CatchableEnemy/CatchableButler.cs
+++ LethalMon/CatchableEnemy/CatchableButler.cs
@@ -1,8 +1,20 @@
-﻿namespace LethalMon.CatchableEnemy;
+﻿using GameNetcodeStuff;
+
+namespace LethalMon.CatchableEnemy;
 
 public class CatchableButler : CatchableEnemy
 {
     public CatchableButler() : base(10, "Butler", 4)
     {
+    }
+
+
+    public override void BeforeCapture(EnemyAI enemyAI, PlayerControllerB player)
+    {
+        base.BeforeCapture(enemyAI, player);
+
+        ButlerEnemyAI butler = (ButlerEnemyAI) enemyAI;
+        butler.ambience1.Stop();
+        butler.ambience2.Stop();
     }
 }
