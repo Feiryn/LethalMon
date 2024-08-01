98cc6924cfab7664dc2b82bc957027a18094cdbd
NiroDev <niros.email@gmail.com>
2024-08-01 21:33:05 +0200

    desync fixes

--- LethalMon/CatchableEnemy/CatchableMasked.cs
+++ LethalMon/CatchableEnemy/CatchableMasked.cs
@@ -1,8 +1,19 @@
-﻿namespace LethalMon.CatchableEnemy;
+﻿using GameNetcodeStuff;
+
+namespace LethalMon.CatchableEnemy;
 
 public class CatchableMasked : CatchableEnemy
 {
     public CatchableMasked() : base(11, "Masked", 6)
     {
+    }
+
+    public override bool CanBeCapturedBy(EnemyAI enemyAI, PlayerControllerB player)
+    {
+        var masked = enemyAI as MaskedPlayerEnemy;
+        if (masked != null)
+            return masked.inSpecialAnimationWithPlayer == null;
+
+        return base.CanBeCapturedBy(enemyAI, player);
     }
 }
