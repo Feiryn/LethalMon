4e8be431f7ba77a109303642a7c4f8570316c09d
Feiryn <98115666+feiryn@users.noreply.github.com>
2024-08-09 13:01:28 +0200

    Make hoarder bug easier to catch

--- LethalMon/CatchableEnemy/CatchableHoarderBug.cs
+++ LethalMon/CatchableEnemy/CatchableHoarderBug.cs
@@ -2,7 +2,7 @@ namespace LethalMon.CatchableEnemy;
 
 public class CatchableHoarderBug : CatchableEnemy
 {
-    public CatchableHoarderBug() : base(2, "Hoarding Bug", 3)
+    public CatchableHoarderBug() : base(2, "Hoarding Bug", 2)
     {
     }
 }
