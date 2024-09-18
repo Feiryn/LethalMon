58871097d5eaa53948322c2a2ea2e2a7d41ff6b1
Feiryn <98115666+feiryn@users.noreply.github.com>
2024-09-18 09:26:30 +0200

    Add Blob PC description

--- LethalMon/CatchableEnemy/CatchableBlob.cs
+++ LethalMon/CatchableEnemy/CatchableBlob.cs
@@ -2,7 +2,7 @@
 
 public class CatchableBlob : CatchableEnemy
 {
-    public CatchableBlob() : base(15, "Hygrodere", 4)
+    public CatchableBlob() : base(15, "Hygrodere", 4, "When a hygrodere is in your team, you can drop items on it, and it will happily carry them for you. Although it's not the fastest, a boombox can help it move faster.\nA failed capture will make the hygrodere grow in size.")
     {
     }
 }
