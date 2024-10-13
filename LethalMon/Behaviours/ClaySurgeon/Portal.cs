b3e8d7455cd86ff558495a9884a69604779e6708
Feiryn <98115666+feiryn@users.noreply.github.com>
2024-10-13 11:34:57 +0200

    Butler's portal (WIP)

--- LethalMon/Behaviours/ClaySurgeon/Portal.cs
+++ LethalMon/Behaviours/ClaySurgeon/Portal.cs
@@ -12,14 +12,22 @@ internal class Portal : MonoBehaviour
 
     public Camera portalCamera;
     
-    Quaternion QuaternionFromMatrix(Matrix4x4 m) { return Quaternion.LookRotation(m.GetColumn(2), m.GetColumn(1)); }
-    
-    Vector4 PosToV4(Vector3 v) { return new Vector4(v.x, v.y, v.z, 1.0f); }
-    
-    Vector3 ToV3(Vector4 v) { return new Vector3(v.x, v.y, v.z); }
-    
     void LateUpdate()
     {
- 
+        Vector3 playerOffsetFromPortal = playerCamera.transform.position - otherPortal.position;
+        portalCamera.transform.position = portal.position + playerOffsetFromPortal;
+        
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.transform.forward;
+        portalCamera.transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        
+        Plane p = new Plane(otherPortal.forward, otherPortal.position);
+        Vector4 clipPlaneWorldSpace = new Vector4(p.normal.x, p.normal.y, p.normal.z, p.distance);
+        Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose(Matrix4x4.Inverse(portalCamera.worldToCameraMatrix)) * clipPlaneWorldSpace;
+
+        Matrix4x4 newMatrix = playerCamera.CalculateObliqueMatrix(clipPlaneCameraSpace);
+        portalCamera.projectionMatrix = newMatrix;
     }
 }
